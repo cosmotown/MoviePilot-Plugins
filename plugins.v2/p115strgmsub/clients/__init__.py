be4bfe5c0cae28d92ad266a7847101b60a4c0467
cosmotown <48054349+cosmotown@users.noreply.github.com>
2026-07-13 01:22:07 +0800

    Add AyclubClient to client modulefeat: export AYCLUB bridge client

--- plugins.v2/p115strgmsub/clients/__init__.py
+++ plugins.v2/p115strgmsub/clients/__init__.py
@@ -2,11 +2,14 @@
 客户端模块
 包含115网盘、PanSou、Nullbr等客户端
 """
+
 from .p115 import P115ClientManager
 from .pansou import PanSouClient
 from .nullbr import NullbrClient
 from .hdhive import HDHiveOpenAPIClient, HDHiveOpenAPIError
 from .openclaw_classifier import OpenClawClassifierClient
+from .ayclub import AyclubClient
+
 
 __all__ = [
     "P115ClientManager",
@@ -15,4 +18,5 @@ __all__ = [
     "HDHiveOpenAPIClient",
     "HDHiveOpenAPIError",
     "OpenClawClassifierClient",
+    "AyclubClient",
 ]
